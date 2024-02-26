9de7f6f17b491730f23f7f70c54f2ba14b293c71
SeM2x <houssem135.302@gmail.com>
2024-02-26 17:52:44 +0100

    17. Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node != node->parent->left)
-        return (node->parent->left);
-    return (node->parent->right);
+	if (node != node->parent->left)
+		return (node->parent->left);
+	return (node->parent->right);
 }
