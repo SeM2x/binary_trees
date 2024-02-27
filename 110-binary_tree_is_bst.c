b76ce3645e0801865c0be7c9fa4a2f9c0e65de3e
SeM2x <houssem135.302@gmail.com>
2024-02-27 21:38:41 +0100

    updated 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_bst_helper - helper function to binary_tree_is_bst
+ *
+ * @node: pointer to the root node
+ * @min: min value of the tree
+ * @max: max value of the tree
+ * Return: 0 or 1.
+ */
+int binary_tree_is_bst_helper(const binary_tree_t *node, const binary_tree_t *min, const binary_tree_t *max)
+{
+	if (!node)
+		return 1;
+
+	if ((min && node->n <= min->n) || (max && node->n >= max->n))
+		return 0;
+
+	return (binary_tree_is_bst_helper(node->left, min, node) &&
+			binary_tree_is_bst_helper(node->right, node, max));
+}
+/**
  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  *
  * @tree: a pointer to the root node of the tree
@@ -8,30 +27,7 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	binary_tree_t **stack, *node, *predecessor;
-	int top = -1;
-
 	if (!tree)
 		return (0);
-
-	stack = NULL;
-	node = (binary_tree_t *)tree;
-	predecessor = NULL;
-
-	while (node || top > -1)
-	{
-		while (node)
-		{
-			stack = realloc(stack, (top + 2) * sizeof(binary_tree_t *));
-			stack[++top] = node;
-			node = node->left;
-		}
-		node = stack[top--];
-		if (predecessor && node->n < predecessor->n)
-			return (0);
-		predecessor = node;
-		node = node->right;
-	}
-
-	return (1);
+	return binary_tree_is_bst_helper(tree, NULL, NULL);
 }
