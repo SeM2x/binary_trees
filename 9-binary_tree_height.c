4f8d5e9c9cbe6ce0cfe43ecaf5a33a27b8340887
SeM2x <houssem135.302@gmail.com>
2024-02-27 09:23:14 +0100

    fixed 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -25,3 +25,29 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (left_height);
 	return (right_height);
 }
+
+/**
+ * binary_tree_is_root - checks if a node is a root
+ *
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+		return (0);
+	return (1);
+}
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node || node->left || node->right)
+		return (0);
+	return (1);
+}
