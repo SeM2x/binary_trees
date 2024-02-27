71bbdb74ab674488f9b7c0d56dd32e84b4d86afd
SeM2x <houssem135.302@gmail.com>
2024-02-27 09:43:47 +0100

    fixed 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (binary_tree_is_leaf(tree))
+	if (!tree->left && !tree->right)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
