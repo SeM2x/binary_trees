5d6eac54b487a31aa3762d6152a4ee5f0a0b03bf
SeM2x <houssem135.302@gmail.com>
2024-02-27 23:59:51 +0100

    updated 11-bst_insert.c

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -13,15 +13,23 @@ bst_t *bst_insert(bst_t **tree, int value)
 
 	if (*tree == NULL)
 	{
-		*tree = binary_tree_node(*tree, value);
+		node = malloc(sizeof(binary_tree_t));
+		if (!node)
+			return (NULL);
+		node->n = value;
+		node->parent = NULL;
+		node->left = NULL;
+		node->right = NULL;
+		*tree = node;
 		return (*tree);
 	}
 
 	if (value > (*tree)->n)
 		node = bst_insert(&(*tree)->right, value);
-
 	else if (value < (*tree)->n)
 		node = (bst_insert(&(*tree)->left, value));
+	else
+		return (NULL);
 
 	if (node && ((*tree)->left == node || (*tree)->right == node))
 		node->parent = *tree;
