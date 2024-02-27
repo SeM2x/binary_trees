a711eb99437f15177cc293690922557c0b4fbf92
SeM2x <houssem135.302@gmail.com>
2024-02-27 10:53:27 +0100

    fixed 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -21,10 +21,25 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree
  * Return: number of nodes
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
 /**
  * binary_tree_is_full - checks if a binary tree is full
  *
