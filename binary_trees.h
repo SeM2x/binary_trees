a0fe99fcd5ebc645f8646b5dc20d2d3e52799e58
SeM2x <houssem135.302@gmail.com>
2024-02-28 07:34:52 +0100

    fixed binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -23,8 +23,6 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s bst_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
