3a6000ab35f92cfe1ea40fe3385af507d1462d15
SeM2x <houssem135.302@gmail.com>
2024-02-28 11:21:13 +0100

    betty

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -10,15 +10,15 @@
 bst_t *_bst_search(const bst_t *tree, int value)
 {
 
-    if (!tree)
-        return (NULL);
+	if (!tree)
+		return (NULL);
 
-    if (tree->n < value)
-        return (_bst_search(tree->right, value));
-    if (tree->n > value)
-        return (_bst_search(tree->left, value));
+	if (tree->n < value)
+		return (_bst_search(tree->right, value));
+	if (tree->n > value)
+		return (_bst_search(tree->left, value));
 
-    return ((bst_t *)tree);
+	return ((bst_t *)tree);
 }
 
 /**
@@ -31,47 +31,47 @@ bst_t *_bst_search(const bst_t *tree, int value)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    bst_t *node, *min;
+	bst_t *node, *min;
 
-    node = _bst_search(root, value);
-    if (!node)
-        return (root);
+	node = _bst_search(root, value);
+	if (!node)
+		return (root);
 
-    if (node->right)
-    {
-        min = node->right;
-        while (min->left)
-            min = min->left;
-        if (min->parent != node)
-            min->parent->left = min->right;
-        else
-            node->right = min->right;
+	if (node->right)
+	{
+		min = node->right;
+		while (min->left)
+			min = min->left;
+		if (min->parent != node)
+			min->parent->left = min->right;
+		else
+			node->right = min->right;
 
-        if (min->right)
-            min->right->parent = min->parent;
+		if (min->right)
+			min->right->parent = min->parent;
 
-        min->left = node->left;
-        if (node->left)
-            node->left->parent = min;
-        min->right = node->right;
-        if (node->right)
-            node->right->parent = min;
-        min->parent = node->parent;
-    }
-    else
-    {
-        min = node->left;
-        if (min)
-            min->parent = node->parent;
-    }
+		min->left = node->left;
+		if (node->left)
+			node->left->parent = min;
+		min->right = node->right;
+		if (node->right)
+			node->right->parent = min;
+		min->parent = node->parent;
+	}
+	else
+	{
+		min = node->left;
+		if (min)
+			min->parent = node->parent;
+	}
 
-    if (!node->parent)
-        root = min;
-    else if (node->parent->left == node)
-        node->parent->left = min;
-    else
-        node->parent->right = min;
+	if (!node->parent)
+		root = min;
+	else if (node->parent->left == node)
+		node->parent->left = min;
+	else
+		node->parent->right = min;
 
-    free(node);
-    return (root);
+	free(node);
+	return (root);
 }
