21fa94234614de426b5f2bced883915ee8056ab2
SeM2x <houssem135.302@gmail.com>
2024-02-29 20:33:53 +0100

    34. AVL - From sorted array

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,39 +1,39 @@
 #include "binary_trees.h"
 
 /**
- * _bst_search - searches for a value in a Binary Search Tree
+ * __bst_search - searches for a value in a Binary Search Tree
  *
  * @tree: a pointer to the root node of the BST to search
  * @value: the value to search in the tree
  * Return: a pointer to the node containing a value equals to value.
  */
-bst_t *_bst_search(const bst_t *tree, int value)
+bst_t *__bst_search(const bst_t *tree, int value)
 {
 
 	if (!tree)
 		return (NULL);
 
 	if (tree->n < value)
-		return (_bst_search(tree->right, value));
+		return (__bst_search(tree->right, value));
 	if (tree->n > value)
-		return (_bst_search(tree->left, value));
+		return (__bst_search(tree->left, value));
 
 	return ((bst_t *)tree);
 }
 
 /**
- * bst_remove - removes a node from a Binary Search Tree
+ * _bst_remove - removes a node from a Binary Search Tree
  *
  * @root: a pointer to the root node of the tree.
  * @value: the value to remove in the tree
  * Return:  a pointer to the new root node of the tree
  * after removing the desired value
  */
-bst_t *bst_remove(bst_t *root, int value)
+bst_t *_bst_remove(bst_t *root, int value)
 {
 	bst_t *node, *min;
 
-	node = _bst_search(root, value);
+	node = __bst_search(root, value);
 	if (!node)
 		return (root);
 	if (node->right)
@@ -111,6 +111,6 @@ avl_t *avl_balance(avl_t *root)
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
-	root = bst_remove(root, value);
+	root = _bst_remove(root, value);
 	return (avl_balance(root));
 }
---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from an array
+ *
+ * @array: a pointer to the first element of the array to be converted
+ * @size: the number of element in the array
+ * Return: a pointer to the root node of the created AVL tree,
+ * or NULL on failure.
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    avl_t *tree, *node;
+
+    if (size % 2 == 0 && size > 2)
+    {
+        tree = sorted_array_to_avl(array, size - 1);
+        node = tree;
+        while (node->right)
+            node = node->right;
+        node->right = binary_tree_node(node, array[size - 1]);
+    }
+    else
+    {
+
+        if (!size)
+            return (NULL);
+
+        if (size == 1)
+            return (binary_tree_node(NULL, array[size / 2]));
+
+        tree = binary_tree_node(NULL, array[size / 2]);
+
+        node = sorted_array_to_avl(array, size / 2);
+        tree->left = node;
+        node->parent = tree;
+
+        if (size / 2 + 1 < size)
+        {
+            node = sorted_array_to_avl(array + size / 2 + 1, size / 2);
+            tree->right = node;
+            node->parent = tree;
+        }
+    }
+    return (tree);
+}
